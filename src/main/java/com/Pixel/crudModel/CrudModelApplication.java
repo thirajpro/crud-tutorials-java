aa2dac94fce59e090b99c1d8fce9cb5f6289d79b
Thiraj Pramodya <thiraj.pro@gmail.com>
2024-04-06 22:54:10 +0530

    crud operations

---
+++ src/main/java/com/Pixel/crudModel/CrudModelApplication.java
@@ -0,0 +1,13 @@
+package com.Pixel.crudModel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudModelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudModelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Pixel/crudModel/CrudModelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Pixel.crudModel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudModelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
